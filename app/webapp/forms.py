314edb22a8371d42a4b65513ed8dfa149167fe75
Irina Ten <virhot17@gmail.com>
2022-10-01 17:33:52 +0600

    Improvement
    Добавлена возможность поиска записи на основной страницы

--- app/webapp/forms.py
+++ app/webapp/forms.py
@@ -6,3 +6,7 @@ class RecordForm(forms.Form):
     name = forms.CharField(max_length=100, required=True, label="Name")
     email = forms.CharField(max_length=100, required=True, label="Email")
     text = forms.CharField(max_length=1500, required=True, label="Description", widget=widgets.Textarea)
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(max_length=100, required=False, label="Search", initial='')
--- app/webapp/view/base.py
+++ app/webapp/view/base.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
+from webapp.forms import SearchForm
 from webapp.models import Book
 
 
 def index_view(request):
-    records = Book.objects.filter(is_delete=False, status=Book.CHOICES[0][0]).order_by('created_at')[::-1]
+    form = SearchForm()
+    form = SearchForm(request.GET)
+    if 'search' in form.data:
+        if form.data['search']:
+            records = Book.objects.filter(name=form.data['search'], is_delete=False, status=Book.CHOICES[0][0]).order_by('created_at')[::-1]
+        else:
+            records = Book.objects.filter(is_delete=False, status=Book.CHOICES[0][0]).order_by('created_at')[::-1]
+    else:
+        records = Book.objects.filter(is_delete=False, status=Book.CHOICES[0][0]).order_by('created_at')[::-1]
     context = {
         "records": records
     }
     return render(request, "index.html", context)
+
