7e27567d3813a72264921499926bf96dbea58dc2
Irina Ten <virhot17@gmail.com>
2022-10-01 12:13:14 +0600

    Improvement
    Введены начальные настройки проекта, coздана модель.

--- app/webapp/admin.py
+++ app/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Book
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'text', 'status')
+    list_filter = ('id', 'name', 'email', 'text', 'status')
+    search_fields = ('name', 'email', 'text')
+    fields = ('id', 'name', 'email', 'text', 'status')
+    readonly_fields = ('id', 'name', 'email')
+
+
+admin.site.register(Book, BookAdmin)
