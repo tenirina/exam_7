c37fce530f1ad78a1a4a05dc9b46db317e553cfb
Irina Ten <virhot17@gmail.com>
2022-10-01 17:54:35 +0600

    Improvement
    Исправлена ошибка поиска.

--- app/webapp/view/base.py
+++ app/webapp/view/base.py
@@ -9,7 +9,7 @@ def index_view(request):
     form = SearchForm(request.GET)
     if 'search' in form.data:
         if form.data['search']:
-            records = Book.objects.filter(name=form.data['search'], is_delete=False, status=Book.CHOICES[0][0]).order_by('created_at')[::-1]
+            records = Book.objects.filter(name__icontains=form.data['search'], is_delete=False, status=Book.CHOICES[0][0]).order_by('created_at')[::-1]
         else:
             records = Book.objects.filter(is_delete=False, status=Book.CHOICES[0][0]).order_by('created_at')[::-1]
     else:
