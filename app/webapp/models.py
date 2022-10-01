10ae3e88e5b63ca0c8e36f3808c9838a514a22d4
Irina Ten <virhot17@gmail.com>
2022-10-01 12:30:28 +0600

    Improvement
    Внесены изменения в базу, согласно ТЗ. Созданы и примененны миграции. Так же внесены тестовые данные и создан файл с фикстурами.

--- app/webapp/models.py
+++ app/webapp/models.py
@@ -11,8 +11,9 @@ class Book(models.Model):
     email = models.EmailField(verbose_name="Email", null=False, blank=False)
     text = models.TextField(verbose_name="Text", max_length=1000, null=False, blank=False)
     created_at = models.DateTimeField(verbose_name="Date of creation", auto_now_add=True, null=False, blank=False)
-    updated_at = models.DateTimeField(verbose_name="Date of update", auto_now=True, null=False, blank=False)
+    updated_at = models.DateTimeField(verbose_name="Date of update", auto_now=True, null=True, blank=False)
     status = models.CharField(verbose_name="Status", null=False, max_length=50, choices=CHOICES, default=CHOICES[0][0])
     is_delete = models.BooleanField(verbose_name="Delete", default=False, null=False)
     deleted_at = models.DateTimeField(verbose_name="Date of delete", null=True, default=None)
+
 
